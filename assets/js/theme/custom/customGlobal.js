85b41a5a44b49e2b530bf249983a031627aae2a9
devbinhduong <quangminh2170@gmail.com>
2023-09-19 00:16:50 +0700

    Done Block Product No CSS

--- assets/js/theme/custom/customGlobal.js
+++ assets/js/theme/custom/customGlobal.js
@@ -21,6 +21,7 @@ export default function(context) {
             /* Add Funcion Here */
             megamenuEditor($context);
             scrollAnimation();
+            homeProductsListID();
 
             if(window.innerWidth > 1024) {
                 menuDesktopAnimate();
@@ -169,6 +170,145 @@ export default function(context) {
                 e.preventDefault();
                 item.classList.toggle("is-open");
             })
+        }
+    }
+    function homeProductsListID() {
+        var $options;
+
+        $(document).ready(function () {
+            const tScroll = $(this).scrollTop();
+
+            homeListId(tScroll);
+        });
+
+        $(window).on('scroll', (e) => {
+            const $target = $(e.currentTarget);
+            const tScroll = $target.scrollTop();
+
+            homeListId(tScroll);
+        });
+
+        function homeListId(tScroll) {
+            if ($('.productsByListId[data-list-id]').length > 0) {
+                $('.productsByListId[data-list-id]').each((index, element) => {
+                    let thisOffetTop = $(element).offset().top - (screen.height) * 1.5;
+                    
+                    if (tScroll > thisOffetTop && !$(element).hasClass('is-loaded')) {
+                        $(element).addClass('is-loaded');
+
+                        var $prodWrapId = $(element).attr('id'),
+                            $wrap,
+                            $listId = $(element).data('list-id');
+                        var homeProColumn = $(element).parents('.product-block').data('columns');
+                        var dots = $(element).parents('.product-block').data('dots');
+                        var limit = $(element).parents('.product-block').data('limit');
+
+                        if ($(element).find('.productCarousel1').length > 0) {
+                            $wrap = $(element).find('.productCarousel1');
+                            $options = {
+                                template: 'custom/homepage/ajax-products-by-list-id-temp-carousel'
+                            };
+                        }
+
+                        if (!$('#product-by-list-' + $prodWrapId + ' .productCarousel1 .productCarousel-slide').length) {
+                            if ($listId.length > 1) {
+                                loadListID($listId, $options, $wrap, homeProColumn, dots, limit);
+                            } else {
+                                loadListID($($listId), $options, $wrap, homeProColumn, dots, limit);
+                            }
+
+                        }
+                    }
+                });
+            }
+        }
+
+        function loadListID(id, options, wrap, homeProColumn, dots, limit) {
+
+            if (id.length <= 1) {
+                var arr = id;
+            } else {
+                var arr = id.split(',');
+            }
+
+            if (id.length > homeProColumn) {
+                var list = arr.slice(0, parseInt(limit));
+            } else {
+                var list = arr;
+            }
+
+            var num = 0;
+
+            for (var i = 0; i <= list.length; i++) {
+                var $prodId = list[i];
+
+                if ($prodId != undefined) {
+                    utils.api.product.getById($prodId, options, (err, response) => {
+                        let hasProd = $(response).find('.card').data('product-id');
+                        if (hasProd != undefined && hasProd != '' && hasProd != null && !$(response).find('.page-content--err').length) {
+                            if (wrap.hasClass('slick-slider')) {
+                                wrap.slick('unslick');
+                                wrap.append(response);
+                            } else {
+                                wrap.append(response);
+                            }
+                        }
+
+                        num++;
+
+                        if (num == list.length) {
+                            wrap.parents('.productsByListId[data-list-id]').find('.custom_productLoading').remove();
+                            wrap.parents('.productsByListId[data-list-id]').addClass('show');
+                            if (wrap.hasClass('productCarousel1')) {
+                                slickCarouselListId(wrap, homeProColumn, dots);
+                            }
+                            return;
+                        }
+                    });
+                }
+            }
+        }
+
+        function slickCarouselListId($wrap, homeProColumn, dots) {
+            $wrap.slick({
+                infinite: false,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: true,
+                arrows: false,
+                mobileFirst: true,
+                nextArrow: "<svg class='slick-next slick-arrow'><use xlink:href='#slick-arrow-next'></use></svg>",
+                prevArrow: "<svg class='slick-prev slick-arrow'><use xlink:href='#slick-arrow-prev'></use></svg>",
+                responsive: [
+                    {
+                        breakpoint: 1280,
+                        settings: {
+                            slidesToShow: homeProColumn,
+                            slidesToScroll: homeProColumn,
+                            arrows: true,
+                            dots: dots
+                        }
+                    },
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: homeProColumn - 2,
+                            slidesToScroll: homeProColumn - 2,
+                            arrows: true,
+                            dots: dots
+                        }
+                    },
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: homeProColumn - 2,
+                            slidesToScroll: homeProColumn - 2,
+                            arrows: false,
+                            dots: true
+                        }
+                    }
+                ]
+            });
         }
     }
 }
